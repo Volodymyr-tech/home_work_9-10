becbfd49e5056f29878b40b2aa750f06c11f5afc
Vladimir Teslenko <vovateslenko.1996.28@gmail.com>
2024-07-30 21:34:35 +0200

    Функция Main для вывода маски номера карты и счета.Учтены вызовы ошибок при вводе данных от пользователя

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ def main():
     user_input = input("Введите номер банковской карты (16 цифр): ")
     try:
         result = get_mask_card_number(user_input)
-        print(f"Маскированный номер карты: {result}")
+        print(f"{result}")
     except ValueError as e:
         print(e)
 
     user_input_2 = input("Введите номер аккаунта (20 цифр): ")
     try:
         result_2 = get_mask_account(user_input_2)
-        print(f"Маскированный номер аккаунта: {result_2}")
+        print(f"{result_2}")
     except ValueError as e:
         print(e)
 
 
 if __name__ == "__main__":
     main()
-
