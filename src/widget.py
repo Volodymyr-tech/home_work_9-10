0193fa23417affba659d3a0a80ba94c250e8abca
Vladimir Teslenko <vovateslenko.1996.28@gmail.com>
2024-07-30 21:27:09 +0200

    Реализована функция для возврата корректного формата даты

--- src/widget.py
+++ src/widget.py
@@ -6,6 +6,7 @@ def mask_account_card(users_input: str) -> str:
     users_input_list = users_input.split()
     name_account_parts = []
     masked_account = ""
+
     for item in users_input_list:
         if item.isalpha():
             name_account_parts.append(item)
@@ -13,12 +14,27 @@ def mask_account_card(users_input: str) -> str:
             masked_account = get_mask_account(item)
         elif item.isdigit and len(item) == 16:
             masked_account = get_mask_card_number(item)
+
     name_account = " ".join(name_account_parts)
     return f"{name_account} {masked_account}"
 
 
-num = input()
-result = mask_account_card(num)
+def get_date(corrent_data: str) -> str:
+    """Функция для возврата даты в корректном формате"""
+    date_list = corrent_data.split("T")
+    del date_list[1]
+    date_string = "".join(date_list)
+    date_list = date_string.split("-")
+
+    year = date_list.pop(0)
+    month = date_list.pop(1)
+
+    date_list.insert(1, year)
+    date_list.insert(0, month)
+
+    return ".".join(date_list)
+
+
+data = input()
+result = get_date(data)
 print(result)
-
-
