7ff6b89ea61251129cbe90b263b050b1cb3992ea
Vladimir Teslenko <vovateslenko.1996.28@gmail.com>
2024-08-14 01:08:53 +0200

    Исправил ошибку вывода транзакции в функции filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -2,25 +2,27 @@ import random
 from typing import Any, Dict, Iterator, List
 
 
-def filter_by_currency(dict_list: List[Dict[str, Any]]) -> Iterator:
+def filter_by_currency(dict_list: List[Dict[str, Any]], corrency: str) -> Iterator:
     """Функция для вывода валюты транзакции"""
     for operation in dict_list:
-        if "operationAmount" in operation:
-            currency = operation["operationAmount"]["currency"]["name"]
-            yield currency
-        elif "operationAmount" or "currency" or "name" not in operation:
-            raise KeyError
+        if "operationAmount" and corrency in operation:
+ #           input_currency = operation["operationAmount"]["currency"]["name"]
+            yield operation
         else:
-            break
+            raise KeyError
+
+        break
+
 
 def transaction_descriptions(dict_transaction: List[Dict[str, Any]]) -> Iterator[str]:
-    '''Функция для возврата описания транзакции'''
+    """Функция для возврата описания транзакции"""
     for info in dict_transaction:
-        if 'description' in info:
+        if "description" in info:
             description = info["description"]
             yield description
         else:
             raise KeyError
+
 
 def card_number_generator(start: int, stop: int) -> Iterator:
     """Функция генерации случайных номеров банковских карт"""
@@ -32,4 +34,3 @@ def card_number_generator(start: int, stop: int) -> Iterator:
             number_str = "0" + number_str
             formated = f"{number_str[0:4]} {number_str[4:8]} {number_str[8:12]} {number_str[12:]}"
             yield formated
-
