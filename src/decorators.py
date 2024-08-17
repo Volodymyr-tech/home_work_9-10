9febade9e2b641d9f96b191d2e308e7d3be0e11f
Vladimir Teslenko <vovateslenko.1996.28@gmail.com>
2024-08-17 18:38:15 +0200

    Добавленны аннотации типов функции декоратора в модуле decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,21 +1,21 @@
 import logging
+from typing import Any, Callable, Optional
 
 logging.basicConfig(level=logging.INFO)
 
 
-def log(filename=None):
-    '''Декоратор для записи выполнения функции в лог файл при наличии имя файла, если параметр None результат работы выводится в консоль'''
-    def decorator(function):
-        def wrapper(*args, **kwargs):
-            log_message = (
-                f"Запуск функции: {function.__name__} ok\n"
-                f"Функция {function.__name__} выполнена.\n"
-            )
+def log(filename: Optional[str] = None) -> Callable[..., Any]:
+    """Декоратор для записи выполнения функции в лог файл при наличии имя файла,
+    если параметр None результат работы выводится в консоль"""
+
+    def decorator(function: Callable[..., Any]) -> Callable[..., Any]:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            log_message = f"Запуск функции: {function.__name__} ok\n" f"Функция {function.__name__} выполнена.\n"
             try:
                 result = function(*args, **kwargs)
 
                 if filename:
-                    with open(filename, 'a', encoding='utf-8') as file:
+                    with open(filename, "a", encoding="utf-8") as file:
                         file.write(log_message + "-" * 40 + "\n")
                 else:
                     print(log_message)
@@ -26,7 +26,7 @@ def log(filename=None):
                 error_message = f"{function.__name__} error: {e.__class__.__name__}.Inputs: {args}, {kwargs}\n"
 
                 if filename:
-                    with open(filename, "a", encoding='utf-8') as file:
+                    with open(filename, "a", encoding="utf-8") as file:
                         file.write(error_message)
                 else:
                     print(error_message)
