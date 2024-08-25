3c2ae9d0db6204c061fd2c7cec7826f139250908
Vladimir Teslenko <vovateslenko.1996.28@gmail.com>
2024-08-26 00:35:52 +0200

    Функция utils покрыта тестами на 93%

---
+++ tests/test_utils.py
@@ -0,0 +1,45 @@
+import pytest
+import json
+from unittest.mock import mock_open, patch
+from src.utils import json_reader  # Замените 'your_module' на имя вашего модуля
+
+
+@pytest.fixture
+def empty_list():
+    return []
+
+@pytest.fixture
+def incorect_path():
+    path = r"C:\Users\Владимир\PycharmProjects\homework_9.1\data"
+    return path
+
+@pytest.fixture
+def dictionary():
+    data = '{"name": "John Smith", "age": 30, "city": "New York"}'
+    return data
+
+
+def test_incorect_path_json_reader(incorect_path, empty_list):
+    assert json_reader(incorect_path) == empty_list
+
+
+def test_getsize_json_reader(tmpdir, empty_list):
+    file = tmpdir.join("test.json")
+    file.write("")
+
+    result = json_reader(str(file))
+
+    # Проверяем, что результатом выполнения функции будет пустой список
+    assert result == empty_list
+
+def test_not_isinstance_json_reader(tmpdir, empty_list, dictionary):
+    file = tmpdir.join("test.json")
+    with open(file,'w', encoding='utf-8') as f:
+        file.write(dictionary)
+
+    result = json_reader(str(file))
+
+    # Проверяем, что результатом выполнения функции будет пустой список
+    assert result == empty_list
+
+
