6b8f56ef2b6be37017bdb5bef2a627d8a9c5ba22
Vladimir Teslenko <vovateslenko.1996.28@gmail.com>
2024-08-25 23:45:22 +0200

    Реализована функция, которая считывает json файл и возвращает список словарей

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import json
+import os.path
+from typing import Any, Dict, Iterator, List
+
+PATH = r"C:\Users\Владимир\PycharmProjects\homework_9.1\data\operations.json"
+
+
+def json_reader(path: str) -> List[Dict[str, Any]]:
+    """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
+    if not os.path.isfile(path):
+        return []
+
+    if os.path.getsize(path) == 0:
+        return []
+
+    with open(path, "r", encoding="utf-8") as file:
+        output_data = json.load(file)
+
+        if not isinstance(output_data, list):
+            return []
+
+        return output_data
